25bf94e02af1524a8d9f53063918554cba85e431
ukashah1 <ukashah60@gmail.com>
2021-08-04 07:55:27 +0000

    REVERT: cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -34,7 +34,6 @@ struct sugov_tunables {
 	struct gov_attr_set attr_set;
 	unsigned int up_rate_limit_us;
 	unsigned int down_rate_limit_us;
-	unsigned int mode_rate_limit_us;
 };
 
 struct sugov_policy {
@@ -533,13 +532,6 @@ static ssize_t down_rate_limit_us_show(struct gov_attr_set *attr_set, char *buf)
 	return sprintf(buf, "%u\n", tunables->down_rate_limit_us);
 }
 
-static ssize_t mode_rate_limit_us_show(struct gov_attr_set *attr_set, char *buf)
-{
-	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
-
-	return sprintf(buf, "%u\n", tunables->mode_rate_limit_us);
-}
-
 static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 				      const char *buf, size_t count)
 {
@@ -547,12 +539,8 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if(tunables->mode_rate_limit_us == 4) {
-		rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	} else {
-		if (kstrtouint(buf, 10, &rate_limit_us))
-			return -EINVAL;
-	}
+	if (kstrtouint(buf, 10, &rate_limit_us))
+		return -EINVAL;
 
 	tunables->up_rate_limit_us = rate_limit_us;
 
@@ -571,12 +559,8 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if(tunables->mode_rate_limit_us == 4) {
-		rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-	} else {
-		if (kstrtouint(buf, 10, &rate_limit_us))
-			return -EINVAL;
-	}
+	if (kstrtouint(buf, 10, &rate_limit_us))
+		return -EINVAL;
 
 	tunables->down_rate_limit_us = rate_limit_us;
 
@@ -588,47 +572,12 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	return count;
 }
 
-static ssize_t mode_rate_limit_us_store(struct gov_attr_set *attr_set,
-					const char *buf, size_t count)
-{
-	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
-	struct sugov_policy *sg_policy;
-	unsigned int mode_rate_limit_us;
-
-	if (kstrtouint(buf, 10, &mode_rate_limit_us))
-		return -EINVAL;
-
-	if (mode_rate_limit_us >= 5 ){
-		mode_rate_limit_us = 0;
-	}
-	tunables->mode_rate_limit_us = mode_rate_limit_us;
-	if (mode_rate_limit_us == 4){
-		// same as defined
-		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	} else if (mode_rate_limit_us == 1) {
-		// battery
-		tunables->down_rate_limit_us = 500;
-		tunables->up_rate_limit_us = 90000;
-	} else if (mode_rate_limit_us == 2) {
-		// balance
-		tunables->down_rate_limit_us = 1000;
-		tunables->up_rate_limit_us = 1000;
-	} else if (mode_rate_limit_us == 3) {
-		// performance
-		tunables->down_rate_limit_us = 90000;
-		tunables->up_rate_limit_us = 500;
-	}
-	return count;
-}
-
 int schedutil_set_down_rate_limit_us(int cpu, unsigned int rate_limit_us)
 {
 	struct cpufreq_policy *policy;
 	struct sugov_policy *sg_policy;
 	struct sugov_tunables *tunables;
 	struct gov_attr_set *attr_set;
-	rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
 
 	policy = cpufreq_cpu_get(cpu);
 	if (!policy)
@@ -646,9 +595,6 @@ int schedutil_set_down_rate_limit_us(int cpu, unsigned int rate_limit_us)
 	}
 
 	tunables = sg_policy->tunables;
-	if(tunables->mode_rate_limit_us == 4) {
-		rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-	}
 	tunables->down_rate_limit_us = rate_limit_us;
 	attr_set = &tunables->attr_set;
 
@@ -672,7 +618,6 @@ int schedutil_set_up_rate_limit_us(int cpu, unsigned int rate_limit_us)
 	struct sugov_policy *sg_policy;
 	struct sugov_tunables *tunables;
 	struct gov_attr_set *attr_set;
-	rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
 
 	policy = cpufreq_cpu_get(cpu);
 	if (!policy)
@@ -690,9 +635,6 @@ int schedutil_set_up_rate_limit_us(int cpu, unsigned int rate_limit_us)
 	}
 
 	tunables = sg_policy->tunables;
-	if(tunables->mode_rate_limit_us == 4) {
-		rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	}
 	tunables->up_rate_limit_us = rate_limit_us;
 	attr_set = &tunables->attr_set;
 
@@ -712,12 +654,10 @@ EXPORT_SYMBOL(schedutil_set_up_rate_limit_us);
 
 static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
 static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
-static struct governor_attr mode_rate_limit_us = __ATTR_RW(mode_rate_limit_us);
 
 static struct attribute *sugov_attributes[] = {
 	&up_rate_limit_us.attr,
 	&down_rate_limit_us.attr,
-	&mode_rate_limit_us.attr,
 	NULL
 };
 
@@ -863,10 +803,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-	// lock rate limit by default
-	tunables->mode_rate_limit_us = 4;
+	tunables->up_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
+	tunables->down_rate_limit_us = cpufreq_policy_transition_delay_us(policy);
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
