54b5e1bfdbe208338419725132211ba48c5a6091
nil <shas45558@gmail.com>
2021-11-27 01:24:48 -0500

    Make anykernel update

--- AnyKernel3-master/anykernel.sh
+++ AnyKernel3-master/anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Neutrino mod Genom Kernel for Redmi 9 by rama982@telegram
+kernel.string=Shas Dream Project
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-#device.name1=lancelot
+device.name1=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
@@ -20,11 +20,9 @@ block=/dev/block/platform/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
@@ -60,4 +58,5 @@ write_boot;
 
 #flash_boot;
 ## end vendor_boot install
+
 
